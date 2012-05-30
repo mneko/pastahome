61c4aea06ae485cf80b0607cedcc11ea62c48bee
mneko <m.the.neko@gmail.com>
2012-05-30 10:00:07 -0500

    hopefully adding jquery
    what am i doing. thanks, stackoverflow

--- magic.js
+++ magic.js
@@ -1,3 +1,11 @@
+//jquery?
+var script = document.createElement('script');
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+
+
 //date work
 function curweekday()
 {
